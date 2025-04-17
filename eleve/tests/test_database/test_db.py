26f092f00ced1e1dde781619201fa6862ed960bd
André Carvalho <alac1984@gmail.com>
2025-04-17 18:56:13 -0300

    Task test created

---
+++ eleve/tests/test_database/test_db.py
@@ -0,0 +1,12 @@
+import pytest
+
+@pytest.mark.asyncio
+async def test_init_db():
+    ...
+    # TODO: create test_init_db
+
+
+@pytest.mark.asyncio
+async def test_get_session():
+    ...
+    # TODO: create test_get_session
